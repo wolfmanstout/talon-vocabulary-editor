5316ecaec45f0fec1c0d509fe49b5a0d634e5e39
James Stout <james.wolf.stout@gmail.com>
2022-05-04 17:39:40 -0700

    Added missing parse_phrase.py.

---
+++ parse_phrase.py
@@ -0,0 +1,40 @@
+# From https://github.com/AndreasArvidsson/andreas-talon/blob/3631f25d426a9fb7526c240cb0c9961ea90072c2/andreas/misc/rephrase.py
+from typing import Optional, Union
+from talon import Context, Module, actions, speech_system
+from talon.grammar import Phrase
+from talon.lib import flac
+
+mod = Module()
+
+phrase_stack = []
+
+
+def on_pre_phrase(d):
+    phrase_stack.append(d)
+
+
+def on_post_phrase(d):
+    phrase_stack.pop()
+
+
+speech_system.register("pre:phrase", on_pre_phrase)
+speech_system.register("post:phrase", on_post_phrase)
+
+
+@mod.action_class
+class Actions:
+    def parse_phrase(phrase: Union[Phrase, str], recording_path: str = ""):
+        """Rerun phrase"""
+        if phrase == "":
+            return
+        current_phrase = phrase_stack[-1]
+        ts = current_phrase["_ts"]
+        start = phrase.words[0].start - ts
+        end = phrase.words[-1].end - ts
+        samples = current_phrase["samples"]
+        pstart = int(start * 16_000)
+        pend = int(end * 16_000)
+        samples = samples[pstart:pend]
+        if recording_path:
+            flac.write_file(recording_path, samples)
+        speech_system._on_audio_frame(samples)
